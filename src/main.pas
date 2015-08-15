6e59f122accd1c0833d7595f4933b8e6628eb0c7
Mikhail Smirnov <mike-smirnoff@yandex.ru>
2015-08-15 09:28:09 +0400

    imp: преобразовал процедуру GetFileLists в функцию GetFileList с параметрами

--- src/main.pas
+++ src/main.pas
@@ -4,7 +4,8 @@ interface
 {$WARN UNIT_PLATFORM OFF}
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, System.Types;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, System.Types,
+  System.IOUtils;
 
 type
   TfmPasAndDfmtoUTF8 = class(TForm)
@@ -16,12 +17,10 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
-    FPasFileList: TStringDynArray;
-    FDfmFileList: TStringDynArray;
     FSL: TStringList;
     procedure SetDir(const Value: String);
     function GetDir: String;
-    procedure GetFileLists;
+    function GetFileList(const DirPath: string; const SearchPattern: string; const SearchOption: TSearchOption): TStringDynArray;
     procedure ConvertFiles(const FilePathList: TStringDynArray);
     procedure ConvertFile(const FilePath: String);
   public
@@ -34,7 +33,7 @@ var
 implementation
 
 uses
-  FileCtrl, System.IOUtils;
+  FileCtrl, System.StrUtils;
 {$R *.dfm}
 
 procedure TfmPasAndDfmtoUTF8.bnSelectDirClick(Sender: TObject);
@@ -63,10 +62,11 @@ begin
 end;
 
 procedure TfmPasAndDfmtoUTF8.bnConvertClick(Sender: TObject);
+var
+  FileList: TStringDynArray;
 begin
-  GetFileLists;
-  ConvertFiles(FPasFileList);
-  ConvertFiles(FDfmFileList);
+  FileList := GetFileList(SourceDir, '*.pas, *.dfm', TSearchOption.soAllDirectories);
+  ConvertFiles(FileList);
   ShowMessage('Готово!');
 end;
 
@@ -86,10 +86,22 @@ begin
   Result := eDir.Text;
 end;
 
-procedure TfmPasAndDfmtoUTF8.GetFileLists;
+function TfmPasAndDfmtoUTF8.GetFileList(const DirPath: string; const SearchPattern: string; const SearchOption: TSearchOption): TStringDynArray;
+var
+  FileSearchPattern: TStringDynArray;
+  i: Integer;
+  SList: TStringList;
+  CurPattern: string;
 begin
-  FPasFileList := TDirectory.GetFiles(SourceDir, '*.pas', TSearchOption.soAllDirectories);
-  FDfmFileList := TDirectory.GetFiles(SourceDir, '*.dfm', TSearchOption.soAllDirectories);
+  SList := TStringList.Create;
+  SList.Clear;
+  FileSearchPattern := SplitString(SearchPattern, ';,');
+  for i := 0 to Length(FileSearchPattern)-1 do
+  begin
+    CurPattern := Trim(FileSearchPattern[i]);
+    SList.AddStrings(TArray<String>(TDirectory.GetFiles(DirPath, CurPattern, SearchOption)));
+  end;
+  Result := TStringDynArray(SList.ToStringArray);
 end;
 
 procedure TfmPasAndDfmtoUTF8.SetDir(const Value: String);
